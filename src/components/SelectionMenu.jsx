5f8ec337679ca0de4ce9b0452fe73bab727c43d5
Agnes Raimundo <agnes.raimundo.rocha@hotmail.com>
2025-12-26 16:35:44 -0300

    fix url de images na SelectionScreen

--- src/components/SelectionMenu.jsx
+++ src/components/SelectionMenu.jsx
@@ -27,7 +27,7 @@ export default function SelectionMenu({ digimons, onSelect }) {
             onClick={() => setSelectedDigimon(digimon)}
           >
             {/*o src contem um new url porque o vercel não identificaria no momento de build que deveria processar se fosse uma string dinâmica */}
-            <img src={new URL(digimon.image, import.meta.url).href} alt={digimon.name} />
+            <img src={new URL(`../..`+digimon.image, import.meta.url).href} alt={digimon.name} />
             <div className="digimon-name">{digimon.name}</div>
           </div>
         ))}
